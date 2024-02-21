a94b549483355e89dc169fc0d18bd7cc4964e30e
Alejandro González Reyes <alexglzrys@gmail.com>
2024-02-21 17:36:13 -0600

    botones de acción -pantalla de edición de producto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -8,13 +8,53 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        // Para asegurar el buen funcionamiento de nuestra App en diferentes dispositivos con altura pequeña, pero sobre todo si estamos concientes que puede aparecer el teclado para ingresar algún tipo de información
-        // Es importante agrupar el contenido principal en widgets que ofrescan scroll (ListView, o SingleChildScrollView)
-        body: SingleChildScrollView(
-      child: Column(
-        children: [ProductImage()],
-      ),
+    return Scaffold(
+        // Colocar el contenido principa en un área segura, esta pantalla no tendrá AppBar
+        body: SafeArea(
+      // Para asegurar el buen funcionamiento de nuestra App en diferentes dispositivos con altura pequeña, pero sobre todo si estamos concientes que puede aparecer el teclado para ingresar algún tipo de información
+      // Es importante agrupar el contenido principal en widgets que ofrescan scroll (ListView, o SingleChildScrollView)
+      child: SingleChildScrollView(
+          child: Column(children: [
+        Stack(children: [
+          const ProductImage(),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: _BackButtonProductScreen(),
+          ),
+          Positioned(top: 20, right: 20, child: _CameraButtonProductScreen())
+        ]),
+      ])),
     ));
+  }
+}
+
+// Widget encargado de mostrar el botón de ir atrás
+class _CameraButtonProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          // Todo: Activar camara o galería
+        },
+        icon: const Icon(
+          Icons.camera_alt_outlined,
+          size: 28,
+          color: Colors.white,
+        ));
+  }
+}
+
+// Widget encargado de mostrar el botón para activar la cámara o galería
+class _BackButtonProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        onPressed: () => Navigator.pop(context),
+        icon: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Colors.white,
+          size: 28,
+        ));
   }
 }
